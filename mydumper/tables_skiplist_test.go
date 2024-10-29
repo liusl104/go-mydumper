721d29adbc6aa72de5dddfe39fd4dea5585ff0e7
Mr <41608720+liusl104@users.noreply.github.com>
2024-10-29 18:00:40 +0800

    Delete mydumper/tables_skiplist_test.go
    Delete mydumper/tables_skiplist_test.go

--- mydumper/tables_skiplist_test.go
+++
@@ -1,9 +0,0 @@
-package mydumper
-
-import "testing"
-
-func TestCheck_skiplist(t *testing.T) {
-	data := []string{"test.t1", "test.t2", "test.a"}
-	// r := check_skiplist(data, "test", "t1")
-	t.Log(data)
-}
